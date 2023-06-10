6bc82a82bedc3a222930e7276d3691e8a65249ac
vicky Chen <vickychenw@gmail.com>
2023-06-10 15:24:52 -0400

    Added user dto

--- apps/user-api/src/user/user.controller.ts
+++ apps/user-api/src/user/user.controller.ts
@@ -1,16 +1,38 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Req, Res, Param } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import {Request, Response} from 'express';
 
 @Controller('user')
 export class UserController {
 
+  //This is the express way - not recommended
   @Get()
-  findAll(): string{
-    return 'Get all users';
+  findAll(@Req() req: Request, @Res() res: Response): Response{
+    console.log('req.url', req.url);
+    return res.send('Hello Vicky');
+  }
+
+  //Recommended way with NestJS
+  @Get(':id')
+  findOne(@Param('id') id) : string{
+    return `Your id is ${id}`;
   }
 
 
   @Post()
-  create():string {
-    return 'create item';
+  create(@Body() reqPayload: CreateUserDto):string {
+    return `${reqPayload.firstName}, ${reqPayload.lastName} created ${JSON.stringify(reqPayload)}`;
   }
+
+  @Delete(':id')
+  delete(@Param() param): string{//different ways of accessing params
+    return `${param.id} deleted!`;
+  }
+
+  @Put(':id')
+  update(@Body() reqPayload: CreateUserDto, @Param ('id') id):string{
+    return `User ${id} updated, name=${reqPayload.firstName}`;
+  }
+
+
 }
