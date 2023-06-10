03246e862721c9a9343f5921d3098f58403e78ad
vicky Chen <vickychenw@gmail.com>
2023-06-10 16:01:46 -0400

    Cleaned the AppModule to import the UserModule, instead of polluting with user controller and user service

--- apps/user-api/src/app/app.module.ts
+++ apps/user-api/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { UserController } from '../user/user.controller';
 import { AppService } from './app.service';
-import { UserService } from '../user/user.service';
+import { UserModule } from '../user/user.module';
 
 //Meeting place for controllers and services
 @Module({
-  imports: [],
-  controllers: [AppController,UserController],
-  providers: [AppService, UserService],
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
